3f0802e7408ef5f2176fcf60900ef584cac9c1c5
Worawut Boontan <worawut@theiconweb.com>
2015-12-23 02:57:22 +0700

    First

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name myresumeApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the myresumeApp
+ */
+angular.module('myresumeApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
